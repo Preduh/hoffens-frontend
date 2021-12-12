d1e9a8ed1408c6ab3e15db73e8fbc9190d365975
Preduh <pedro.lucas5860@gmail.com>
2021-12-12 00:23:04 -0300

    Adapting to mobile

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Section = styled.section`
   background: url("./assets/merlinDeath-min.png");
   background-repeat: no-repeat;
-  background-size: 1366px;
+  background-size: auto 100%;
   width: 100%;
   height: 100vh;
 
