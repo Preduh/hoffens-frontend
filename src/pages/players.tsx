cca47c89cb43aa802777eb286d8587a3b63d25d4
Preduh <pedro.lucas5860@gmail.com>
2021-12-23 19:11:50 -0300

    show players in players page

--- src/pages/players.tsx
+++ src/pages/players.tsx
@@ -1,39 +1,37 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 import { Players } from "../styles/pages/Players";
+import { api } from "../utils/api";
+
+interface UserData {
+  id: string;
+  username: string;
+}
 
 const PlayersPage: NextPage = () => {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    api.get("/users").then(response => setUsers(response.data));
+  }, []);
+
   return (
     <>
       <Head>
         <title>Jogadores</title>
       </Head>
       <Players id="players">
-        <div className="player">
-          <div className="avatar" />
-          <label>Jogador1</label>
-        </div>
-        <div className="player">
-          <div className="avatar" />
-          <label>Jogador2</label>
-        </div>
-        <div className="player">
-          <div className="avatar" />
-          <label>Jogador3</label>
-        </div>
-        <div className="player">
-          <div className="avatar" />
-          <label>Jogador4</label>
-        </div>
-        <div className="player">
-          <div className="avatar" />
-          <label>Jogador5</label>
-        </div>
-        <div className="player">
-          <div className="avatar" />
-          <label>Jogador6</label>
-        </div>
+        {users &&
+          users.map((user: UserData) => {
+            return (
+              <div className="player" key={user.id}>
+                <div className="avatar" />
+                <label>{user.username}</label>
+              </div>
+            );
+          })}
       </Players>
     </>
   );
